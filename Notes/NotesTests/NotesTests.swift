cff8532f55d54f0d36ca4bf37301e24418bbd247
Волнухин Виктор <volnukhin.v@rfc1918.private.ip.localhost>
2019-07-06 10:50:40 +0300

    Repair unit-tests in settings

--- Notes/NotesTests/NotesTests.swift
+++ Notes/NotesTests/NotesTests.swift
@@ -7,27 +7,29 @@
 //
 
 import XCTest
+@testable import Notes
 
 class NotesTests: XCTestCase {
+    
+    var standartNote: Note!
+    var fullNote: Note!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        standartNote = Note(title: "Today things", content: "Today todo list:", importance: .usual)
+        fullNote = Note(uid: UUID().uuidString, title: "My mom dirthday party", content: "1. Invite grand parents;\n2.Cook tasty cookies;\n3.Buy balloons.", color: .purple, importance: .important, selfDestructionDate: Date().addingTimeInterval(1000000))
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    /// 1. Проверка всех полей структуры Note на иммутабельность.
+    func testNote_mainFieldsNotNil() {
+        XCTAssertNotNil(standartNote.uid)
+        XCTAssertNotNil(standartNote.title)
+        XCTAssertNotNil(standartNote.content)
     }
 
 }
